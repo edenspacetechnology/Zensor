b0f4bac331e5f5c87cedfba51242eb28a78aa640
Wai Leng <shizuma.onesama@gmail.com>
2016-01-21 22:17:48 +0800

    fix search result display to censoring also

--- data/nonono.js
+++ data/nonono.js
@@ -2,14 +2,30 @@ var bruce = /bruce\sjenner/i
 var cait = /caitlyn\sjenner/i
 
 self.port.on("contentReady",function(){
-	var elements = document.getElementsByClassName("entry");
-	var count = 0
-	async.each(elements,function(elem){
+	var searchElements = document.getElementsByClassName("search-result")
+	var entryElements = document.getElementsByClassName("entry");
+	//var searchCount = 0
+	async.each(searchElements,function(elem){
 		if (elem.textContent.match(bruce) || elem.textContent.match(cait) ){
 			//elem.style.display = 'none';
 			elem.textContent = "Blockedddddddddddddddd by /u/livebeta "
-			count++;
-			console.log(count + " triggered words were blocked")	
+			//searchCount++;
+			//console.log(searchCount + " triggered words were blocked")	
+		}
+		
+
+	},function(err){
+		if(err){
+			alert("Async operation went oops!")	;
+		}
+		
+	});
+	async.each(entryElements,function(elem){
+		if (elem.textContent.match(bruce) || elem.textContent.match(cait) ){
+			//elem.style.display = 'none';
+			elem.textContent = "Blockedddddddddddddddd by /u/livebeta "
+			//searchCount++;
+			//console.log(searchCount + " triggered words were blocked")	
 		}
 		
 
